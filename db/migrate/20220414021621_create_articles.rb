7be8cb69806b9bf92bc3925b0226a370336029d8
WMs784 <wms784.app@gmail.com>
2022-04-14 11:41:47 +0900

    build portfolio

--- db/migrate/20220414021621_create_articles.rb
+++ db/migrate/20220414021621_create_articles.rb
@@ -1,4 +1,4 @@
-class CreateArticles < ActiveRecord::Migration[7.0]
+bin/rails db:migrateclass CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
       t.string :title
